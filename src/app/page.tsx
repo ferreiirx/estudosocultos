48c768bf53f55232fdac98e4bdef6e0fb00f1782
Thales Ferreira <thales.martinho@hotmail.com>
2025-09-13 02:09:25 +0000

    O problema do scroll e nisso aqui:
    class="absolute left-0 right-0 top-0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,9 @@ import { Footer } from '@/components/landing/footer';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-between overflow-x-hidden">
+    <div className="relative flex min-h-screen w-full flex-col items-center justify-between overflow-x-hidden">
       <div className="absolute inset-0 -z-10 h-full w-full bg-background bg-[linear-gradient(to_right,#8080800c_1px,transparent_1px),linear-gradient(to_bottom,#8080800c_1px,transparent_1px)] bg-[size:24px_24px]">
-        <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[550px] w-[550px] rounded-full bg-primary/20 opacity-30 blur-[120px]"></div>
+        <div className="absolute left-1/2 -translate-x-1/2 top-0 -z-10 m-auto h-[550px] w-full max-w-[550px] rounded-full bg-primary/20 opacity-30 blur-[120px]"></div>
       </div>
       <main className="w-full">
         <HeroSection />
