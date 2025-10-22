2651e567ce08063cbd9270a9e5d18946e4ebacf1
Thales Ferreira <thales.martinho@hotmail.com>
2025-10-22 10:57:03 +0000

    remova o script de utms e o pixel do facebook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,27 +19,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '4272167596338520');
-            fbq('track', 'PageView');
-          `}
-        </Script>
       </head>
       <body className="font-body antialiased">
-        <noscript>
-          <img height="1" width="1" style={{display: 'none'}}
-            src="https://www.facebook.com/tr?id=4272167596338520&ev=PageView&noscript=1"
-          />
-        </noscript>
         {children}
         <Toaster />
       </body>
